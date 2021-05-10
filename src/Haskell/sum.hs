61686fdf26c0fc37ecc83e5da5676a0a44bdb5d2
Sam Buters <sambuters@gmail.com>
2021-05-10 20:00:48 -0300

    Adding SYSV to Haskell

--- src/Haskell/sum.hs
+++ src/Haskell/sum.hs
@@ -5,12 +5,19 @@ import Data.Bits
 
 import qualified Data.ByteString as BS
 
-
 cyclicShiftR x = ( shiftR x 1 ) + ( shiftL ( (.&.) x 1 ) 15 )
+
+sumList [] sum = sum
+sumList (x:xs) sum = sumList xs (x + sum)
 
 bsdchecksum [] checksum = checksum
 bsdchecksum (x:xs) checksum = bsdchecksum xs new_checksum
                             where new_checksum = (.&.) ((cyclicShiftR checksum) + x) 0xffff
+
+sysvchecksum bytes = ((.&.) r 0xffff) + (shiftR r 16)
+                     where
+                        s = sumList bytes 0
+                        r = ((.&.) s 0xffff) + (shiftR ((.&.) s 0xffffffff) 16)
 
 main = do
     args <- getArgs
@@ -21,3 +28,4 @@ main = do
     let bytes = map fromIntegral (BS.unpack byteString) :: [Int]
 
     print $ bsdchecksum bytes 0
+    print $ sysvchecksum bytes
